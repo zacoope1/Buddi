09a24aa833f31c94c69b7f9a162c4e5e8bd523c5
zacoope1 <zacoope1@asu.edu>
2021-09-18 19:53:12 -0700

    Add environment variable handler.

--- buddi-backend/src/index.ts
+++ buddi-backend/src/index.ts
@@ -4,11 +4,11 @@ import cors from "cors";
 import helmet from "helmet";
 
 type ENVIRONMENT_TYPE = "PROD" | "DEV"
-
 const SELECTED_ENVIRONMENT: ENVIRONMENT_TYPE = "DEV";
-
-const ENVRIONMENT_PATH = {PROD: ".env", "DEV": ".devenv"}
+const ENVRIONMENT_PREFIX = {PROD: "PROD_", "DEV": "DEV_"}
+export const getEnvironementVariable = (environmentVariableName: string): string | undefined => process.env[`${ENVRIONMENT_PREFIX[SELECTED_ENVIRONMENT]}${environmentVariableName}`]
 
 dotenv.config();
 
 
+console.log(getEnvironementVariable("SERVER_PORT"));
