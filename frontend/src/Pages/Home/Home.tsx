c5af49fffe1ae4d639836eb8594323731841244f
Zac Cooper <Zac.Cooper@neudesic.com>
2021-11-10 08:37:44 -0700

    Add MongoDB Connection to Backend.

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useUserContext } from '../../Contexts/UserContext';
 import { Login } from './Login';
 import { useHistory } from 'react-router-dom';
 import { PROFILE } from '../../Routes';
+import { Button } from '../../Components/Common/Button';
+import { Get } from '../../Services/HttpClient';
 
 export const Home = (): JSX.Element => {
   const { isLoggedIn } = useUserContext();
@@ -13,6 +15,7 @@ export const Home = (): JSX.Element => {
 const HomeComponent = () => {
   const { user } = useUserContext();
   const history = useHistory();
+  const [content, setContent] = useState<string | null>(null);
 
   useEffect(() => {
     user && user.displayName === null && history.push(PROFILE);
@@ -21,7 +24,18 @@ const HomeComponent = () => {
   return (
     <>
       <h3>Welcome{user && `, ${user.displayName}`}!</h3>
-      {user && <></>}
+      {user && (
+        <>
+          <Button
+            onClick={async () => {
+              Get('/user', await user.getIdToken()).then(res => setContent(res.data.message));
+            }}
+          >
+            Get
+          </Button>
+          {content && <p>{content}</p>}
+        </>
+      )}
     </>
   );
 };
